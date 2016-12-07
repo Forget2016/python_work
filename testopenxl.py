055e697541ac611e86db74a797d751af14a4653e
Forget2016 <j661767@mvrht.com>
2016-12-07 22:05:45 +0800

    handle columns

--- testopenxl.py
+++ testopenxl.py
@@ -5,8 +5,23 @@
 
 from openpyxl import Workbook
 
-#creating the excel file
+#creating the excel file,if want to use the format inference
+#need to use the parameter guess_types
 wb = Workbook()
+wb = Workbook(guess_types=True) #allow use the type and format
 
 #the other way is loading the file
-wb = Workbook("testfile.xlsx")
+wb = load_workbook("testfile.xlsx")
+
+#the second step create the sheet,insert at the end
+ws = wb.create_sheet("lastsheet")
+#the other way insert the sheet at the first position
+ws = wb.create_sheet("firstsheet", 0)
+
+#change the title by using the title property of sheet
+ws.title = "changetitle"
+
+
+#the cell of sheet actives, for example change the A4 value
+ws("A4") = 12
+ws.cell(row=4, column=1, value=12)
